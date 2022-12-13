ac8c2214951fa935eb5488df38187bb4957bff03
Gabriel de Oliveira Lopes <lopesgabriel0199@gmail.com>
2022-12-13 19:22:34 -0300

    fix: fixes user already in use message

--- src/modules/users/useCases/createUser/CreateUserController.ts
+++ src/modules/users/useCases/createUser/CreateUserController.ts
@@ -8,8 +8,18 @@ class CreateUserController {
 
   handle(request: Request, response: Response): Response {
     const { email, name } = request.body;
-    const newUser = this.createUserUseCase.execute({ email, name });
-    return response.status(201).json(newUser);
+    try {
+      const newUser = this.createUserUseCase.execute({ email, name });
+      return response.status(201).json(newUser);
+    } catch (err) {
+      let message = "Failed to process request";
+
+      if (err.message === "User e-mail already in use") {
+        message = err.message;
+      }
+
+      return response.status(400).json({ error: message });
+    }
   }
 }
 
